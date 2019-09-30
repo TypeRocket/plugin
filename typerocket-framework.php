7fd50f99470fd42f8f33fb646c3acb6dbb983e3c
Kevin Dees <kvdees@gmail.com>
2019-09-30 14:09:15 -0400

    update to core v4.0.57

--- typerocket-framework.php
+++ typerocket-framework.php
@@ -3,7 +3,7 @@
 Plugin Name: TypeRocket Framework 4
 Plugin URI: https://typerocket.com/
 Description: TypeRocket Framework - A WordPress Framework To Empower Your Development
-Version: 4.0.9
+Version: 4.0.10
 Author: Robojuice
 Author URI: https://robojuice.com/
 License: GPLv3 or later
@@ -29,7 +29,7 @@ class TypeRocket_Framework {
 
         $this->path = plugin_dir_path(__FILE__);
         define('TR_AUTO_LOADER', '__return_false');
-        define('TR_PLUGIN_VERSION', '4.0.9');
+        define('TR_PLUGIN_VERSION', '4.0.10');
         register_activation_hook( __FILE__, array($this, 'activation') );
         add_action('admin_notices',  array($this, 'activation_notice') );
         add_action('plugins_loaded', array($this, 'plugins_loaded'));
