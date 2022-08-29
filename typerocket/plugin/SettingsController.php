4cb2cd1aa220a2408f8a65fce1879de29f42cf3d
Kevin Dees <kvdees@gmail.com>
2022-08-29 16:11:16 -0400

    update settings template engine

--- typerocket/plugin/SettingsController.php
+++ typerocket/plugin/SettingsController.php
@@ -2,11 +2,12 @@
 namespace TypeRocketPlugin;
 
 use TypeRocket\Controllers\Controller;
+use TypeRocket\Template\TemplateEngine;
 
 final class SettingsController extends Controller
 {
     public function typerocket()
     {
-        return tr_view(__DIR__ . '/../../admin.php');
+        return tr_view(__DIR__ . '/../../admin.php')->setEngine(TemplateEngine::class);
     }
 }
